fe5f7f46bd1c9f25ebd86cb8c119e6bbfb375f56
chuk <chukmunnlee@gmail.com>
2018-07-31 16:44:53 +0800

    form field examples

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,11 +4,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 const MODULES = [
     FlexLayoutModule,
     MatToolbarModule, MatListModule, MatSidenavModule,
-    MatIconModule
+    MatIconModule, MatInputModule, MatFormFieldModule,
+    MatButtonModule
 ];
 
 @NgModule({
